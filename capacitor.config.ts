20e469098a5b8dbf62f0a9c13226fefd61ec55ea
Filipe Mallmann Siota <filipe.siota@diarioescola.com.br>
2024-06-10 23:14:09 -0300

    feat: ionic template

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Sophia',
+  webDir: 'dist'
+};
+
+export default config;
